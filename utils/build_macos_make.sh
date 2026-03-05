7174a9288e829ddf12c691102d670e362e24c36b
Kononenko Oleg <o.kononenko@qiwi.com>
2026-03-05 10:12:44 +0300

    Remove unused

--- utils/build_macos_make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd nginx-1.19.3/
-
-make
--- utils/build_win_make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "This command does not run anything!"
-echo "You have to execute this commands manually"
-echo "1. Open VS x64 Native Tools Command Prompt for VS 2017"
-echo "2. cd nginx-1.19.3"
-echo "3. nmake"
-echo "Take a coffe and wait until is done"
